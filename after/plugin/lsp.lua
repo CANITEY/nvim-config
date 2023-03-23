61d9dcd85f62019fad8835ce2681de67696bea85
mohamedmed01345 <mohamedmed01345@gmail.com>
2023-03-23 04:01:40 +0200

    fixes

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -20,10 +20,6 @@ lsp.configure('lua-language-server', {
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_sources = cmp.config.sources({
-        {name = 'luasnip'}
-    }
-)
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item({cmp_select}),
   ['<C-n>'] = cmp.mapping.select_next_item({cmp_select}),
